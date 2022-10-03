4374a02b0e61f804f6bd7d48c5b890a63ae4c47e
ShotaHachisuka <koroku@ShotaHachisukanoMacBook-Pro.local>
2022-10-04 00:01:02 +0900

    開発環境構築

--- app/source/ChatBotProject/ChatBotProject/asgi.py
+++ app/source/ChatBotProject/ChatBotProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for demo_project project.
+ASGI config for ChatBotProject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demo_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatBotProject.settings')
 
 application = get_asgi_application()
