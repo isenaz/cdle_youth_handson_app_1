0846471754cf11886a2e76fda14ae45ef0662a4d
ShotaHachisuka <koroku@ShotaHachisukanoMacBook-Pro.local>
2022-10-04 12:03:19 +0900

    htmlファイルをloginとroomで分けた url.pyの設定を追加

--- app/source/ChatBotProject/ChatBotProject/urls.py
+++ app/source/ChatBotProject/ChatBotProject/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("cdle_chatbot/", include("ChatBotApp.urls")),
 ]
+
+# 開発時の設定
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
