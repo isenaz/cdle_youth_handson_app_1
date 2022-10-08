1b146fada2bc7d4d54631b1db6d2717e27eeca9d
mochi <yuuyan9946@gmail.com>
2022-10-08 18:01:33 +0900

    views, urls, modelsにおいて基礎となる部分を作成

--- app/source/ChatBotProject/ChatBotApp/models.py
+++ app/source/ChatBotProject/ChatBotApp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class ChatBotModel(models.Model):
+    chat_bot_type = models.IntegerField()
+    created_at = models.DateTimeField(auto_now=True, auto_now_add=False)
