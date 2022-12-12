b9a42b37b053bbdba70adc493d6923b68d49be4b
mochi <yuuyan9946@gmail.com>
2022-12-12 06:18:43 +0000

    コメントの追加

--- ChatBotApp/models.py
+++ ChatBotApp/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 # Create your models here.
 class ChatBotModel(models.Model):
-    chat_bot_type = models.IntegerField()
     created_at = models.DateTimeField(auto_now=True, auto_now_add=False)
     # チャットbot名のデータが必要かもしれない
-    # チャットbotのアイコン画像パスが必要かもしれない
+    # 後で: チャットbotのアイコン画像パスが必要かもしれない
     def __str__(self):
         return f'type{self.chat_bot_type}'
