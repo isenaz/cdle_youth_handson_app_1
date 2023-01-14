dec6a77d1a673f073f9c5bc3221ef20e73286df1
mochi <yuuyan9946@gmail.com>
2023-01-14 10:53:41 +0000

    余分なファイルを削除

--- old_ChatBotApp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class ChatBotModel(models.Model):
-    created_at = models.DateTimeField(auto_now=True, auto_now_add=False)
-    # チャットbot名のデータが必要かもしれない
-    # 後で: チャットbotのアイコン画像パスが必要かもしれない
-    def __str__(self):
-        return f'type{self.chat_bot_type}'
--- old_ChatBotApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "chatbot_app"
-urlpatterns = [
-    path('chatbot/', views.ChatBot.as_view(), name='chatbot'),
-]
